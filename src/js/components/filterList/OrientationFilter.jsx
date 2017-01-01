a297f11b6e6bf983879ee668602e1c44e2e3182e
marta <martasap@users.noreply.github.com>
2017-01-02 00:42:04 +0100

    Quickfix - Orientation console error

--- src/js/components/filterList/OrientationFilter.jsx
+++ src/js/components/filterList/OrientationFilter.jsx
@@ -1,27 +1,18 @@
 import React from 'react'
 
 export default class OrientationFilter extends React.Component {
-    constructor(){
-        super();
-        this.state = {selected: ''};
-
-        this.handleChange = this.handleChange.bind(this);
-    }
-    handleChange(e) {
-        this.setState(e.target.value);
-    }
     render() {
         return (
             <form className="OrientationFilter">
                 <label>
-                    <input type="radio" value="Horizontal" onChange={this.handleChange} name="Orientation" />
+                    <input type="radio" value="Horizontal" name="Orientation" />
                     <div className="OrientationInputWrapper">
                         <div className="HorizontalFilter"></div>
                     </div>
                     <div className="HorizontalLabel">horizontal</div>
                 </label>
                 <label>
-                    <input type="radio" value="Vertical" onChange={this.handleChange} name="Orientation" />
+                    <input type="radio" value="Vertical" name="Orientation" />
                     <div className="OrientationInputWrapper">
                         <div className="VerticalFilter"></div>
                     </div>
